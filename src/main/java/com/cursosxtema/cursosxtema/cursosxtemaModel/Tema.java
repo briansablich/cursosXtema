7b12ce231b84b63004d9905c971391eab369d194
briansablich <116393964+briansablich@users.noreply.github.com>
2024-12-29 16:57:54 -0300

    Added Models

---
+++ src/main/java/com/cursosxtema/cursosxtema/cursosxtemaModel/Tema.java
@@ -0,0 +1,11 @@
+package com.cursosxtema.cursosxtema.cursosxtemaModel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Tema {
+    private Long idTema;
+    private String nombre;
+    private String descripcion;
+}
