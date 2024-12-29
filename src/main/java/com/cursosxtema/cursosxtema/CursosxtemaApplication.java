3121a14057caf31ead2bbf56beab83335f59bd07
briansablich <116393964+briansablich@users.noreply.github.com>
2024-12-29 16:24:54 -0300

    First commit

---
+++ src/main/java/com/cursosxtema/cursosxtema/CursosxtemaApplication.java
@@ -0,0 +1,13 @@
+package com.cursosxtema.cursosxtema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosxtemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosxtemaApplication.class, args);
+	}
+
+}
